bd412c33a51cad8106d7af1973465d65a2c71d5d
TenoNadar <nadarteno3@gmail.com>
2018-10-20 13:51:44 +0530

    tested

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const socketio = require('socket.io');
 
 // Init Nexmo
 const nexmo = new Nexmo({
-    apiKey: 'YOURAPIKEY',
-    apiSecret: 'YOURAPISECRET'
+    apiKey: 'd9d53b1f',
+    apiSecret: 'wCpqE2QIlQq3BoQX'
   }, { debug: true });
   
   // Init app
@@ -32,21 +32,20 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
     // res.send(req.body);
     // console.log(req.body);
-    const { number, text } = req.body;
+    const  number = req.body.number;
+    const text  = req.body.text;
     nexmo.message.sendSms(
-        'YOURVURTUALNUMBER', number, text, { type: 'unicode' },
+        '917021169359', number, text, { type: 'unicode' },
         (err, responseData) => {
           if(err) {
             console.log(err);
           } else {
-            const { messages } = responseData;
-            const { ['message-id']: id, ['to']: number, ['error-text']: error  } = messages[0];
             console.dir(responseData);
             // Get data from response
             const data = {
-              id,
-              number,
-              error
+              id:responseData.messages[0]['message-id'],
+              number:responseData.messages[0]['to']
+            
             };
     
             // Emit to the client
