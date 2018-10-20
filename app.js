a1837403c05fdc3410d47ea672c837dd892c94d9
TenoNadar <nadarteno3@gmail.com>
2018-10-20 12:14:48 +0530

    nexmo initialized

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ const ejs = require('ejs');
 const Nexmo = require('nexmo');
 const socketio = require('socket.io');
 
+// Init Nexmo
+const nexmo = new Nexmo({
+    apiKey: 'YOURAPIKEY',
+    apiSecret: 'YOURAPISECRET'
+  }, { debug: true });
+  
+  // Init app
+  const app = express();
+
+
