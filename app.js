17d7a9696cd3af630a65bd3eec428cbde4e92c84
TenoNadar <nadarteno3@gmail.com>
2018-10-20 12:15:26 +0530

    template setup

--- app.js
+++ app.js
@@ -13,4 +13,19 @@ const nexmo = new Nexmo({
   // Init app
   const app = express();
 
+// Template engine setup
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+
+// Public folder setup
+app.use(express.static(__dirname + '/public'));
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Index route
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
